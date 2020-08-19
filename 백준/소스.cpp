fa2bdfd9f75754ea930587972689dd493b8269e4
hhhminme <kenma409@gmail.com>
2020-08-19 13:49:58 +0900

    stack

--- 백준/소스.cpp
+++ 백준/소스.cpp
@@ -1,20 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <algorithm>
+#include <cstring>
 using namespace std;
-int main(void) {
-	int num;
-	int time[1001];
-	cin >> num; 
-	for (int i = 0; i < num; i++) {
-		cin >> time[i];
-	}
-	sort(time, time + num);
+
+int stack[10000]; 
+int main() {
+	int num; 
+	int topNum = -1; 
+	char str[BUFSIZ]; 
+	int i,X; 
 	
-	int sum = 0; 
-	for (int i = 0; i < num; i++) {
-		for (int j = 0; j <= i; j++) {
-			sum += time[j];
+	cin >> num; 
+	for (i = 0; i <= num; i++) {
+		cin >> str; 
+		if (strcmp(str, "push")==0) {
+			cin >> X;
+			stack[++topNum] = X;
+			
+		}
+		if (strcmp(str, "pop")==0) {
+			if (topNum < 0) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << stack[topNum - 1] << '\n';
+				topNum--;
+			}
+			
+		}
+		if (strcmp(str, "size")==0) {
+			cout << topNum+1 << '\n';
+		}
+		if (strcmp(str, "empty")==0) {
+			if (topNum < 0) {
+				cout << 1 << '\n';
+			}
+			else {
+				cout << 0 << '\n';
+			}
+		}
+		if(strcmp(str, "top")) {
+			if (topNum < 0){
+				cout << -1 << '\n';
+			}
+			else {
+				cout << stack[topNum] << '\n';
+			}
 		}
 	}
-	cout << sum; 
 }
